42d98fc8e1209cedad575718b647880301e89786
Prasann Singhal <prasannsinghal.dev@gmail.com>
2024-03-11 19:33:23 -0500

    Big refactor on API code to adopt Greg's thing. Note that old version should be a sub-version of this thing.

--- src/rmapi.py
+++ src/rmapi.py
@@ -11,14 +11,14 @@ from contextlib import nullcontext
 from torch.utils.data import DataLoader
 from datasets import Dataset
 from statistics import mean
-
+import numpy as np
 
 from utils.rl_utils import (
     load_models, 
 )
 from utils.args.api_args import APIArguments
 from utils.data.dataproc import add_row_index, create_missing_folders_for_file
-from utils.data.api_data import trainheuristic_data, reuse_batchdata
+from utils.data.api_data import reuse_batchdata
 from utils.api_utils import get_gold_and_log
 
 from rlhfutils.data import load_manual, tokenize_dset
@@ -59,131 +59,97 @@ def train():
         input_texts = data.get("texts", [])
         # for logging / later use
         metrics['all_texts'].extend(input_texts)
-        scores = []
-        tokinps = []
-        tokattns = []
+        metrics['cinds'].extend([metrics['call_count']]*len(input_texts))
         
+        print("call count is ", metrics['call_count'])
+        
+        # compute scores on current batch, get all scores, inps, attentions
         # TODO depending on speed may need to turn off
         for i in range(0, len(input_texts), script_args.batch_size):
-            varupdata = (metrics['call_count']%script_args.callratio)==0
-            ncont = ((varupdata and (script_args.tracking==False)) and script_args.noupdates==False)
-            if ncont: 
-                print("WARNING, GRAD UPDATES ON GIVEN DATA")
             # if we want to do the variance updates
-            with nullcontext() if ncont else torch.no_grad():
+            with torch.no_grad():
                 inps = tokenizer(input_texts[i:i+script_args.batch_size], padding=True, truncation=True, return_tensors="pt").to(reward_model.device)
-                tokinps.extend(inps.input_ids.detach())
-                tokattns.extend(inps.attention_mask.detach())
                 rewards = reward_model(input_ids=inps.input_ids, attention_mask=inps.attention_mask)[0]
-                scores.extend(rewards.detach().squeeze(-1).tolist())
+                # keep a working list of things we pass in here, they're going to get used later in training
+                metrics['inpids'].extend(inps.input_ids.detach())
+                metrics['masks'].extend(inps.attention_mask.detach())
+                metrics['rscores'].extend(rewards.detach().squeeze(-1).tolist())
+        
+        returnscos = metrics['rscores'][-len(input_texts):]
+        
+        assert len(metrics['masks'])==len(metrics['rscores'])
+        assert len(metrics['masks'])==len(metrics['all_texts'])
+        
+        # we're ready to do some kind of active update
+        if script_args.tracking: # TODO HACK this needs to be an actual condition
+            inds = list(range(0, len(metrics['rscores']), 2))
+            # can use either random-based or slightly more complex confidence thing for selecting examples to use in active updates
+            if script_args.relab_criteria=="conf":
+                diffs = list([abs(float(metrics['rscores'][i]-metrics['rscores'][i+1])) for i in range(0, len(metrics['rscores']), 2)])
+                inds = list([inds[i] for i in np.argsort(diffs)])
+            elif "rand" in script_args.relab_criteria:
+                random.shuffle(inds)
             
+            inds = inds[:int(len(inds)*script_args.relabel_ratio)]
+            print("we're going to now train with: ", len(inds), " preference pairs")
             
-            loss = 0 
-            print("call count is ", metrics['call_count'])
-            
-            # Do a batch of validation on normal data
-            # if varupdata==False: #(metrics['call_count']%script_args.oldratio)==0:
-                
-            if varupdata:
-                tmploss=0
-                if "indiv" in script_args.diffunct:
-                    print("doing indiv thing")
-                    # TODO update this based on mini-batch size stuff, varmax with oversample
-                    for i in range(0, len(rewards), 2):
-                        tmploss = tmploss + torch.sigmoid(torch.abs(rewards[i]-rewards[i+1]))
-                    # do the variance loss on everything in the batch
-                    tmploss = tmploss * (2/len(rewards)) * -1
-                elif "batch" in script_args.diffunct:  
-                    print("doing dffunct thing")
-                    # trick to view all the differences
-                    pairwise_diff = rewards.unsqueeze(1) - rewards.unsqueeze(0)
-                    # stuff to log for the first time we try this
-                    if metrics['call_count']==1:
-                        print(pairwise_diff)
-                    # use secrets loss
-                    tmploss = torch.sigmoid(torch.abs(pairwise_diff)).sum()
-                    tmploss = tmploss * (1/(pairwise_diff.shape[1]**2)) * -1
-                # NOTE a way to compose the normal update and the new update things
-                loss = loss + tmploss
-                # HACK moving the get_gold_and_log call
-                # get_gold_and_log(rewards, inps, input_texts, tokenizer, reward_model, script_args, i, metrics)
-                # external method call, note that this is important for preparing data with some methods
-            if ((metrics['call_count']+1)%script_args.oldratio)==0:
-                print("old data update")
-                try:
-                    inputs = next(loaddata)
-                except: 
-                    loaddata = iter(train_dataloader)
-                    inputs = next(loaddata)
-                rewards_j = reward_model(input_ids=inputs["input_ids_j"].to(reward_model.device), attention_mask=inputs["attention_mask_j"].to(reward_model.device))[0]
-                rewards_k = reward_model(input_ids=inputs["input_ids_k"].to(reward_model.device), attention_mask=inputs["attention_mask_k"].to(reward_model.device))[0]
-                
-                rdiff = rewards_j - rewards_k
-                
-                loss = -nn.functional.logsigmoid(rdiff).mean()
+            # once we've figured out that we want to update things, let's now select something with a strategy, get golds, etc. 
+            get_gold_and_log(inds, tokenizer, script_args, metrics)
 
-            # we're doing a standard preference update w.r.t the original dataset
-            if loss!=0 and script_args.noupdates==False:
-                print("replayloss ", float(loss.detach()))
-                optimizer.zero_grad()
-                loss.backward()
-                torch.nn.utils.clip_grad_norm_(reward_model.parameters(), max_norm=1.0)
-                optimizer.step()
-                print(("prompt var step" if "indiv" in script_args.diffunct=='indiv' else "variance step") if varupdata else "retrain step")
-        
-        assert len(tokinps)==len(scores)
-        get_gold_and_log(scores, {'input_ids':tokinps, 'attention_mask':tokattns}, input_texts, tokenizer, reward_model, script_args, 0, metrics)
-        
-        # NOTE got rid of weighted value function thing
-        metrics['threshsum'].append(mean([abs(scores[i]-scores[i+1]) for i in range(0, len(scores), 2)]))
-        metrics['threshsum'] = metrics['threshsum'][-100:] # HACK hardcoded to 100 for now
-            
-        if script_args.trainheur:
-            print("trainheur")
-            trainheuristic_data(metrics, tokenizer, script_args, reward_model.device)
-        
-        if (script_args.tracking or script_args.trainheur) and (len(metrics['extradata'])>0):
+            if script_args.noupdates: 
+                return jsonify(returnscos)
             # take random data points from what we've been messing with
             random.shuffle(metrics['extradata'])
-            # newdata = metrics['extradata'][script_args.batch_size*script_args.redo_batches:]
-            tmpdset = Dataset.from_list(metrics['extradata'][:script_args.batch_size*script_args.redo_batches]) # HACK this thing is redundant somehow
+            tmpdset = Dataset.from_list(metrics['extradata'])
+            
             # NOTE kicking shuffling back in for stability
             tmpdset = DataLoader(tmpdset, batch_size=script_args.batch_size, shuffle=True, collate_fn=RewardDataCollatorWithPadding(tokenizer=tokenizer, max_length=script_args.max_length))
-            readd_inds = []
-            cind = 0
-            # do the whole extra adding thing
-            for batch in tmpdset:
+            
+            # cind = 0 readd_inds = []
+            for e in script_args.update_epochs:
                 
-                rewards_j = reward_model(input_ids=batch["input_ids_j"].to(reward_model.device), attention_mask=batch["attention_mask_j"].to(reward_model.device))[0]
-                rewards_k = reward_model(input_ids=batch["input_ids_k"].to(reward_model.device), attention_mask=batch["attention_mask_k"].to(reward_model.device))[0]
-                
-                rdiff = rewards_j - rewards_k # NOTE main bug was here, things should now be fine
-                loss = -nn.functional.logsigmoid(rdiff).mean()
-                
-                ndiff = rdiff.detach()
-                
-                # TODO make sure that the adding is happening correctly
-                reuse_batchdata(ndiff, tokenizer, batch, metrics, readd_inds, script_args, cind, rewards_j, rewards_k)
-                cind += script_args.batch_size
-                if script_args.noupdates==False:
-                    print("activeloss ", float(loss.detach()))
+                # do the whole extra adding thing
+                for idx, batch in enumerate(tmpdset):
+                    
+                    rewards_j = reward_model(input_ids=batch["input_ids_j"].to(reward_model.device), attention_mask=batch["attention_mask_j"].to(reward_model.device))[0]
+                    rewards_k = reward_model(input_ids=batch["input_ids_k"].to(reward_model.device), attention_mask=batch["attention_mask_k"].to(reward_model.device))[0]
+                    
+                    rdiff = rewards_j - rewards_k # NOTE main bug was here, things should now be fine
+                    loss = -nn.functional.logsigmoid(rdiff).mean()
                     optimizer.zero_grad()
                     loss.backward()
                     torch.nn.utils.clip_grad_norm_(reward_model.parameters(), max_norm=1.0)
                     optimizer.step()
-            # add back data that needs to be added back
-            newdata = list([metrics['extradata'][ind] for ind in readd_inds])
-            print("iterated on ", len(metrics['extradata']), " now have ", len(newdata))
-            metrics['extradata'] = newdata
-                
-        print("scores", scores)
+                    
+                    # ndiff = rdiff.detach() NOTE leaving this here in case we want XP replay back
+                    # reuse_batchdata(ndiff, tokenizer, batch, metrics, readd_inds, script_args, cind, rewards_j, rewards_k)
+                    # cind += script_args.batch_size
+                    
+                    print(idx, ": epoch ", e, ": activeloss ", float(loss.detach()))
+                    
+            # reset everything now
+            resetkeys = ["extradata", 'inpids', 'masks', 'rscores', 'all_texts', 'cinds']
+            for r in resetkeys:
+                metrics[r] = []
             
+            # add back data that needs to be added back
+            # newdata = list([metrics['extradata'][ind] for ind in readd_inds])
+            # print("iterated on ", len(metrics['extradata']), " now have ", len(newdata))
+            # metrics['extradata'] = newdata
+            
+            metrics['label_count']+=len(inds)
+            if metrics['label_count']>=script_args.stopupdates:
+                print("DISABLING RM UPDATES FROM THIS POINT")
+                script_args.noupdates=True
+                reward_model.eval()
+                
         if metrics['call_count'] % script_args.save_freq == 0 and (script_args.noupdates==False):
             
             create_missing_folders_for_file(script_args.output_dir+"/step_"+str(metrics['call_count'])+"/")
             reward_model.save_pretrained(script_args.output_dir+"/step_"+str(metrics['call_count']))
+        
             
-        return jsonify(scores)
+        return jsonify(returnscos)
     
 if __name__ == '__main__':
     lock = threading.Lock()
@@ -199,9 +165,8 @@ if __name__ == '__main__':
 
     set_seed(script_args.seed)
     
-    metrics = {'call_count':0, 'label_count':0, 'all_texts':[], 'threshsum':[], 'extradata':[], 'logdata':[], 'reuses':{}}
+    metrics = {'call_count':0, 'label_count':0, 'all_texts':[], 'all_scores':[], 'inps':[], 'masks':[], 'rscores':[], 'cinds':[], 'extradata':[], 'logdata':[], 'reuses':{}}
 
-    
     print("goldreward is ", script_args.goldreward)
     if script_args.trainable:
         tokenizer, reward_model = load_models(script_args, "train")
--- src/utils/api_utils.py
+++ src/utils/api_utils.py
@@ -9,63 +9,32 @@ Given a set of rewards, / inputs, score everything with the gold function, updat
 
 TODO clean up call code and expand the functionality a bit
 """
-def get_gold_and_log(rewards, inps, input_texts, tokenizer, reward_model, script_args, baseind, metrics):
+def get_gold_and_log(inds, tokenizer, script_args, metrics):
+    # special case for this thing
     if "contdist" in metrics.keys():
         rw.likemod = metrics['contdist'][0]
         rw.liketok = metrics['contdist'][1]
         rw.slikemod = metrics['contdist'][2]
         rw.sliketok = metrics['contdist'][3]
+        
     allngs = []
     acc = 0
     totnew = 0
-    # we're doing some kind of active update
-    if script_args.tracking: 
-        inds = list(range(0, len(rewards), 2))
-        # can use either random-based or slightly more complex confidence thing for selecting examples to use
-        if script_args.relab_criteria=="conf":
-            diffs = list([abs(float(rewards[i]-rewards[i+1])) for i in range(0, len(rewards), 2)])
-            inds = list([inds[i] for i in np.argsort(diffs)])
-            print("conf")
-        elif "rand" in script_args.relab_criteria:
-            random.shuffle(inds)
-            print("rand")
-        inds = inds[:int(len(inds)*script_args.relabel_ratio)]
-        
-    for ind in range(0, len(rewards), 2):
-        if "ultra" in script_args.goldreward:
-            newgs = []
-        else:
-            newgs = get_synth_rewards(input_texts[baseind+ind:baseind+ind+2], script_args.goldreward)
-            if newgs[0]!=newgs[1]:
-                acc += 1 if ((newgs[0]>newgs[1])==(rewards[ind]>rewards[ind+1])) else 0
-                totnew +=1
+    for ind in range(0, len(metrics['rscores']), 2):
+        # TODO just make this a script_arg, maybe have a hard limit for the ultra version? 
+        getgold = (ind in inds) if ("ultra" in script_args.goldreward) else True
+        # to prevent going bankrupt
+        newgs = get_synth_rewards(metrics['all_texts'][ind:ind+2], script_args.goldreward) if getgold else None
+        allngs.append(newgs)
 
         tmp = {
-            'texts':input_texts[baseind+ind:baseind+ind+2],
-            'rewards':[float(f) for f in rewards[ind:ind+2]],
+            'texts':metrics['all_texts'][ind:ind+2],
+            'rewards':[float(f) for f in metrics['rscores'][ind:ind+2]],
             'golds':newgs,
-            # 'thresh':0,
-            'labelled':metrics['label_count'],
-            'step':metrics['call_count']
+            'call':metrics['cinds'][ind:ind+2]
         }
-        
-        # TODO this needs to have an abs on it I think? Currently most examples are getting used
-        # NOTE this used to be a thresholding thing, now base stuff on selection criteria code (above)
+        # Add to data now
         if ind in inds:
-            # only get UF gold labels for selected examples to avoid going bankrupt
-            if "ultra" in script_args.goldreward:
-                newgs = get_synth_rewards(input_texts[baseind+ind:baseind+ind+2], script_args.goldreward)
-                tmp['golds'] = newgs
-                if newgs[0]!=newgs[1]:
-                    acc += 1 if ((newgs[0]>newgs[1])==(rewards[ind]>rewards[ind+1])) else 0
-                    totnew +=1
-                
-            metrics['label_count']+=1
-            if metrics['label_count']>=script_args.stopupdates:
-                # NOTE if we label past a certain limit then stop grad updates
-                script_args.noupdates=True
-                reward_model.eval()
-            # tmp['thresh'] = float(script_args.labelthresh*metrics['threshsum'])
             # TODO this may have been a big bug?
             allngs.append(newgs)
             # track how much extra data is needed (TODO at the beginning this will make some noise)
@@ -75,15 +44,12 @@ def get_gold_and_log(rewards, inps, input_texts, tokenizer, reward_model, script
             elif newgs[1]>newgs[0]:
                 jind = ind+1
                 kind = ind
-            else: 
-                append_dict_to_jsonl(tmp, script_args.logfile, metrics['logdata'])
-                continue
             if script_args.tracking:
                 # create new dataset on the fly
-                metrics['extradata'].insert(0, {"input_ids_j":inps['input_ids'][jind], "attention_mask_j":inps['attention_mask'][jind],
-                                    "input_ids_k":inps['input_ids'][kind], "attention_mask_k":inps['attention_mask'][kind]})
+                metrics['extradata'].insert(0, {"input_ids_j":metrics['inpids']['input_ids'][jind], "attention_mask_j":metrics['masks']['attention_mask'][jind],
+                                    "input_ids_k":metrics['inpids']['input_ids'][kind], "attention_mask_k":metrics['masks']['attention_mask'][kind]})
                 # keep track of how much each datapoint gets reused
-                keyval = tokenizer.decode(inps['input_ids'][jind], skip_special_tokens=True)+tokenizer.decode(inps['input_ids'][kind], skip_special_tokens=True)
+                keyval = tokenizer.decode(metrics['inpids']['input_ids'][jind], skip_special_tokens=True)+tokenizer.decode(metrics['inpids']['input_ids'][kind], skip_special_tokens=True)
                 if keyval not in metrics['reuses']:
                     metrics['reuses'][keyval] = 0
                 metrics['reuses'][keyval] = metrics['reuses'][keyval]+1
