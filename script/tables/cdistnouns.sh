0ef6dcee3e8133264f19fe43cf3b376ec0c0992c
Prasann Singhal <prasannsinghal.dev@gmail.com>
2024-03-14 15:57:26 -0500

    Better noun table setup

--- script/tables/cdistnouns.sh
+++ script/tables/cdistnouns.sh
@@ -43,6 +43,32 @@ defaults() {
 defaults
 export BASEMODEL="outputs/models/nouns/smalldpo"
 
+export ATYPE="rand"
+export UEPOCHS=3
+export APBSIZE=16
+export GREWARD="nouns"
+
+export DPOBATCHSIZE=32
+export MBSIZE=32
+export GBSIZE=32
+
+export SAMPN=$((32*5))
+export RELABELS=$((5))
+export CUDA_VISIBLE_DEVICES=1
+# noupdateapi "bagofwords" "bowsynth50knozeros" "bowtiny_rm" "reprodtest" 5000
+nohup sh script/newupdateapi.sh "nouns" "" "tiny_rm" "randnoun_newalgo_100small_200testv3" 5009 & 
+sleep 20
+# Other commands
+export CUDA_VISIBLE_DEVICES=0,4
+sh script/dpoplus_script.sh "nouns" "ultra" "http://127.0.0.1:5009/train" 29520 "randnoun_newalgo_100small_200testv3"
+jobs
+pkill -f "randnoun_newalgo_100small_200testv3"
+jobs
+
+# NOUNS
+defaults
+export BASEMODEL="outputs/models/nouns/smalldpo"
+
 export ATYPE="conf"
 export UEPOCHS=3
 export APBSIZE=16
@@ -53,43 +79,19 @@ export MBSIZE=32
 export GBSIZE=32
 
 export SAMPN=$((32*50))
-export RELABELS=$((25))
+export RELABELS=$((50))
 export CUDA_VISIBLE_DEVICES=1
 # noupdateapi "bagofwords" "bowsynth50knozeros" "bowtiny_rm" "reprodtest" 5000
-nohup sh script/newupdateapi.sh "nouns" "" "tiny_rm" "confnoun_newalgo_100small_200test" 5009 & 
+nohup sh script/newupdateapi.sh "nouns" "" "tiny_rm" "confnoun_newalgo_100small_200testv3" 5009 & 
 sleep 20
 # Other commands
 export CUDA_VISIBLE_DEVICES=0,4
-sh script/dpoplus_script.sh "nouns" "ultra" "http://127.0.0.1:5009/train" 29520 "confnoun_newalgo_100small_200test"
+sh script/dpoplus_script.sh "nouns" "ultra" "http://127.0.0.1:5009/train" 29520 "confnoun_newalgo_100small_200testv3"
 jobs
-pkill -f "confnoun_newalgo_100small_200test"
+pkill -f "confnoun_newalgo_100small_200testv3"
 jobs
 
-# NOUNS
-defaults
-export BASEMODEL="outputs/models/nouns/smalldpo"
 
-export ATYPE="rand"
-export UEPOCHS=3
-export APBSIZE=16
-export GREWARD="nouns"
-
-export DPOBATCHSIZE=32
-export MBSIZE=32
-export GBSIZE=32
-
-export SAMPN=$((32*50))
-export RELABELS=$((25))
-export CUDA_VISIBLE_DEVICES=1
-# noupdateapi "bagofwords" "bowsynth50knozeros" "bowtiny_rm" "reprodtest" 5000
-nohup sh script/newupdateapi.sh "nouns" "" "tiny_rm" "randnoun_newalgo_100small_200test" 5009 & 
-sleep 20
-# Other commands
-export CUDA_VISIBLE_DEVICES=0,4
-sh script/dpoplus_script.sh "nouns" "ultra" "http://127.0.0.1:5009/train" 29520 "randnoun_newalgo_100small_200test"
-jobs
-pkill -f "randnoun_newalgo_100small_200test"
-jobs
 
 # # BEST TECHNIQUE (ALL IN ONE)
 # defaults
