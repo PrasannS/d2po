50282f67b26faf1a7d35aff79f0845c457cf5b9b
Prasann Singhal <prasannsinghal.dev@gmail.com>
2024-03-21 19:38:55 -0500

    Actual DPO api run ready now

--- script/tables/bownoundpoapirm.sh
+++ script/tables/bownoundpoapirm.sh
@@ -5,8 +5,8 @@ export CFG=src/configs/ppo_2gpu.yaml
 export STEPS=2000
 export SUPDATES=10000000
 export SEED=2
-export KEEPLONG=0
 export MLEN=50
+export KEEPLONG=0
 
 defaults() {
     export BASEMODEL="outputs/models/bagofwords/bowtiny_dpo"
@@ -61,7 +61,7 @@ export SAMPN=$((32*5))
 export RELABELS=$((2))
 export CUDA_VISIBLE_DEVICES=0
 # noupdateapi "bagofwords" "bowsynth50knozeros" "bowtiny_rm" "reprodtest" 5000
-nohup sh script/newupdateapi.sh "nouns" "" "tiny_rm" "confnoun_newalgo_2_5_seed2_dpoapi" 5003 & 
+nohup sh script/newupdateapi.sh "nouns" "" "smalldpo" "confnoun_newalgo_2_5_seed2_dpoapi" 5003 & 
 sleep 20
 # Other commands
 export CUDA_VISIBLE_DEVICES=1,2
@@ -70,3 +70,30 @@ jobs
 pkill -f "confnoun_newalgo_2_5_seed2_dpoapi"
 jobs
 
+# CDIST GET A BASELINE AT SMALLER INTERV LEVEL
+defaults
+export KEEPLONG=10
+
+export ATYPE="rand"
+export UEPOCHS=3
+export APBSIZE=2
+export GREWARD="contrastivedistill"
+export BASEMODEL="outputs/models/contrastivedistill/smalldpo"
+
+
+export DPOBATCHSIZE=32
+export MBSIZE=32
+export GBSIZE=32
+
+export SAMPN=$((32*10))
+export RELABELS=$((5))
+export CUDA_VISIBLE_DEVICES=3
+# noupdateapi "bagofwords" "bowsynth50knozeros" "bowtiny_rm" "reprodtest" 5000
+nohup sh script/newupdateapi.sh "contrastivedistill" "" "smalldpo" "rand_cdist_10_5_activefixseed2" 5010 & 
+sleep 20
+# Other commands
+export CUDA_VISIBLE_DEVICES=4,5
+sh script/dpoplus_script.sh "contrastivedistill" "outputs/data/contrastivedistill/wikionpprompts200k" "http://127.0.0.1:5010/train" 29522 "rand_cdist_10_5_activefixseed2"
+jobs
+pkill -f "rand_cdist_10_5_activefixseed2"
+jobs
